51534d2298b2e4e3daae7b69f634f5937e08d052
Amdrew Walsh <andrew.jordan.walsh@gmail.com>
2021-06-13 00:55:27 -0500

    started making student header file

---
+++ cmake-build-debug/CMakeFiles/ClassRoster.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClassRoster.dir/main.cpp.obj"
+  "CMakeFiles/ClassRoster.dir/roster.cpp.obj"
+  "CMakeFiles/ClassRoster.dir/student.cpp.obj"
+  "ClassRoster.exe"
+  "ClassRoster.exe.manifest"
+  "ClassRoster.lib"
+  "ClassRoster.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClassRoster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
